9170597723cb74f1cdfe6251c6905a8aaeafaecc
Calindra Build Server <calindra@Calindra-Build-Server.local>
2025-09-30 14:04:21 -0300

    chore: :bookmark: release

--- Package.swift
+++ Package.swift
@@ -13,12 +13,11 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/hyperoslo/Cache.git", from: "6.0.0"),
     ],
     targets: [
         .target(
             name: "EitriDependencies",
-            dependencies: ["Cache"]),
+            dependencies: []),
 
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
